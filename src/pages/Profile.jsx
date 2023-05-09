c6aba01c521ad141d4310c2f523afeec8dee9dbd
John Lee <john.lee@wfs.aero>
2023-05-09 17:41:15 -0400

    section 15 - 98

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,8 +3,10 @@ import React, { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 function Profile() {
   const auth = getAuth();
@@ -92,6 +94,11 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+        </Link>
       </main>
     </div>
   );
